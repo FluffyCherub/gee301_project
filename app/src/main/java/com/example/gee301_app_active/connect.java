0b7b7193117b030fce4b781a5978ad37ef420660
FluffyCherub <73240956+FluffyCherub@users.noreply.github.com>
2021-04-05 00:59:44 +0100

    Revert "Revert "Sign-up Added""
    This reverts commit 10327d0cafce4c51765afff9e355cafd270364c9.

--- app/src/main/java/com/example/gee301_app_active/connect.java
+++ app/src/main/java/com/example/gee301_app_active/connect.java
@@ -1,5 +1,7 @@
 package com.example.gee301_app_active;
 
+import org.json.JSONObject;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
--- app/src/main/java/com/example/gee301_app_active/login_activity.java
+++ app/src/main/java/com/example/gee301_app_active/login_activity.java
@@ -1,17 +1,16 @@
 package com.example.gee301_app_active;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
+import android.annotation.SuppressLint;
+import android.widget.Button;
+import android.widget.TextView;
 import org.json.JSONObject;
+
 
 public class login_activity extends AppCompatActivity {
 
@@ -64,6 +63,7 @@ public class login_activity extends AppCompatActivity {
                                     if(cS.status == true && cS.ResponseCode == 200){
                                         Snackbar.make(login_view, "Success!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
                                         TextView login_status = (TextView) findViewById(R.id.login_status);
+
                                         login_status.setText("Login Succeeded");
 
                                     }
@@ -82,12 +82,8 @@ public class login_activity extends AppCompatActivity {
                     System.out.println("GENERAL EXCEPTION");
                     e.printStackTrace(System.out);
                 }
-
-
-
-
-
             }
         });
+
     }
 }
